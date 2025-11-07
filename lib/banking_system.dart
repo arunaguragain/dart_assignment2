890de9607d2a8136d3a1f24b32755291e85078a2
arunaguragain <guragainaruna@gmail.com>
2025-11-07 19:26:12 +0545

    saving account done

---
+++ lib/banking_system.dart
@@ -0,0 +1,42 @@
+abstract class BankAccount {
+  int _accountNumber;
+  String _accountHolderName;
+  double _balance;
+
+  BankAccount(this._accountHolderName, this._accountNumber, this._balance);
+
+  double withdraw(double amount);
+  double deposit(double amount);
+  void displayAccountInfo() {
+    print("Account info is");
+    print("Account number: $_accountNumber");
+    print("Account Holder Name: $_accountHolderName");
+    print("Balance: $_balance");
+  }
+
+  int get getAccountNumber {
+    return _accountNumber;
+  }
+
+  set setAccountNumber(int accountNumber) {
+    _accountNumber = accountNumber;
+  }
+
+  String get getAccountHolderName {
+    return _accountHolderName;
+  }
+
+  set setAccountHolderName(String accountHolderName) {
+    _accountHolderName = accountHolderName;
+  }
+
+  double get getBalance {
+    return _balance;
+  }
+
+  set setBalance(double balance) {
+    _balance = balance;
+  }
+}
+
+
---
+++ lib/saving_account.dart
@@ -0,0 +1,33 @@
+import 'package:dart_assignment2/banking_system.dart';
+
+class SavingAccount extends BankAccount {
+  double minimumBalance = 500;
+  double interest = 0.02;
+  int withdrawalLimit = 3;
+  int withdrawalcount = 0;
+
+  SavingAccount(super.accountHolderName, super.accountNumber, super.balance);
+
+  @override
+  double deposit(double amount) {
+    setBalance = getBalance + amount;
+    print("$amount deposited successfully");
+    return getBalance;
+  }
+
+  @override
+  double withdraw(double amount) {
+    if (withdrawalLimit > withdrawalcount) {
+      if (getBalance < amount || getBalance - amount < minimumBalance) {
+        print("There is no sufficient amount");
+      } else {
+        setBalance = getBalance - amount;
+        print("$amount withdrawn successfully");
+        withdrawalcount += 1;
+      }
+    } else {
+      print("Withdrawal limit crossed");
+    }
+    return getBalance;
+  }
+}
